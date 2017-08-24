28075f7e2800922d29b1b0202ab09760fd6d2108
GyrateDark <GyrateDark@users.noreply.github.com>
2017-08-24 14:15:18 -0400

    Added a region class and renamed it to Card_region

---
+++ TCG_Creator/Card_Region.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TCG_Creator
+{
+    public enum IMAGE_OPTIONS
+    {
+        Crop,
+        Fill,
+        Stretch
+    }
+
+    public class Card_Region
+    {
+        public Rectangle location;
+        public string description;
+        public int id;
+
+        public string text;
+        public Typeface text_typeface;
+        public bool decrease_text_size_to_fit = true;
+
+        public ImageSource background_image;
+        public IMAGE_OPTIONS background_image_filltype;
+
+        public void draw_region(ref BitmapImage img, Rectangle location)
+        {
+            ImageBrush background_img_brush = new ImageBrush(background_image);
+            
+            //background_img_brush.
+        }
+    }
+}
