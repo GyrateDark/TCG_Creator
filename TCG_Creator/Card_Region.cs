9e6fe9508b7013757131b39c4a3e0d4003d2814a
GyrateDark <GyrateDark@users.noreply.github.com>
2017-08-25 02:10:05 -0400

    Part 1 of adding background drawing to regions

--- TCG_Creator/Card_Region.cs
+++ TCG_Creator/Card_Region.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace TCG_Creator
 {
     public enum IMAGE_OPTIONS
     {
-        Crop,
         Fill,
-        Stretch
+        Letterbox,
+        Unified_Fill,
+        None
     }
 
     public class Card_Region
     {
-        public Rectangle location;
+        public Rect location;
         public string description;
         public int id;
 
@@ -25,13 +26,31 @@ namespace TCG_Creator
         public Typeface text_typeface;
         public bool decrease_text_size_to_fit = true;
 
-        public ImageSource background_image;
-        public IMAGE_OPTIONS background_image_filltype;
+        public ImageSource std_background_image;
+        public IMAGE_OPTIONS background_image_filltype = IMAGE_OPTIONS.None;
 
-        public void draw_region(ref BitmapImage img, Rectangle location)
+        public void draw_region(ref DrawingGroup base_img)
         {
-            ImageBrush background_img_brush = new ImageBrush(background_image);
+            if (background_image_filltype != IMAGE_OPTIONS.None)
+            {
+                ImageBrush image_brush = new ImageBrush(std_background_image);
+                if (background_image_filltype == IMAGE_OPTIONS.Fill)
+                    image_brush.Stretch = Stretch.Fill;
+                else if (background_image_filltype == IMAGE_OPTIONS.Letterbox)
+                    image_brush.Stretch = Stretch.Uniform;
+                else if (background_image_filltype == IMAGE_OPTIONS.Unified_Fill)
+                    image_brush.Stretch = Stretch.UniformToFill;
+
+                BitmapImage background = image_brush;
+                
+                ImageDrawing drawing = new ImageDrawing(, location);
+            }
+
             
+
+            drawing
+
+
             //background_img_brush.
         }
     }
