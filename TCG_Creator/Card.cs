7c77a65a44214967bd4cf498b6e5cdcc78838dd2
GyrateDark <GyrateDark@users.noreply.github.com>
2017-08-26 13:40:53 -0500

    Finished Drawing and Cards

--- TCG_Creator/Card.cs
+++ TCG_Creator/Card.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace TCG_Creator
 {
@@ -11,5 +14,24 @@ namespace TCG_Creator
         public List<Card_Region> regions;
         public int id;
         public string name;
+
+        public DrawingGroup render_card(Rect location)
+        {
+            DrawingGroup card_drawing = new DrawingGroup();
+
+            foreach (Card_Region i in regions)
+            {
+                Rect draw_location = new Rect();
+
+                draw_location.X = location.X + i.ideal_location.X * location.Width;
+                draw_location.Y = location.Y + i.ideal_location.Y * location.Height;
+                draw_location.Width = location.Width * i.ideal_location.Width;
+                draw_location.Height = location.Height * i.ideal_location.Height;
+
+                card_drawing.Children.Add(i.draw_region(draw_location));
+            }
+
+            return card_drawing;
+        }
     }
 }
--- TCG_Creator/Card_Region.cs
+++ TCG_Creator/Card_Region.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Windows.Media.Imaging;
 
 namespace TCG_Creator
@@ -18,7 +19,8 @@ namespace TCG_Creator
 
     public class Card_Region
     {
-        public Rect location;
+        // 0-1 location of region on  a card of width 1 and height 1
+        public Rect ideal_location;
         public string description;
         public int id;
 
@@ -26,11 +28,15 @@ namespace TCG_Creator
         public Typeface text_typeface;
         public bool decrease_text_size_to_fit = true;
 
+        public Brush text_brush;
+
         public ImageSource std_background_image;
         public IMAGE_OPTIONS background_image_filltype = IMAGE_OPTIONS.None;
 
-        public void draw_region(ref DrawingGroup base_img)
+        public DrawingGroup draw_region(Rect draw_location)
         {
+            DrawingGroup reg_img = new DrawingGroup();
+
             if (background_image_filltype != IMAGE_OPTIONS.None)
             {
                 ImageBrush image_brush = new ImageBrush(std_background_image);
@@ -41,17 +47,34 @@ namespace TCG_Creator
                 else if (background_image_filltype == IMAGE_OPTIONS.Unified_Fill)
                     image_brush.Stretch = Stretch.UniformToFill;
 
-                BitmapImage background = image_brush;
-                
-                ImageDrawing drawing = new ImageDrawing(, location);
+                GeometryDrawing image_rec_drawing = new GeometryDrawing(image_brush, new Pen(Brushes.Transparent, 0), new RectangleGeometry(draw_location));
+
+                reg_img.Children.Add(image_rec_drawing);
             }
 
-            
+            if (text != null)
+            {
+                GlyphRun text_glyph_run = new GlyphRun();
 
-            drawing
+                GlyphTypeface auto_typeface;
+                bool auto_convert_typeface = text_typeface.TryGetGlyphTypeface(out auto_typeface);
 
+                if (auto_convert_typeface)
+                {
+                    text_glyph_run.GlyphTypeface = auto_typeface;
+                }
+                else
+                {
+                    text_glyph_run.Characters = text.ToCharArray();
+                    text_glyph_run.DeviceFontName = text_typeface.FontFamily.Source;
+                }
 
-            //background_img_brush.
+                GlyphRunDrawing text_drawing = new GlyphRunDrawing(text_brush, text_glyph_run);
+
+                reg_img.Children.Add(text_drawing);
+            }
+
+            return reg_img;
         }
     }
 }
